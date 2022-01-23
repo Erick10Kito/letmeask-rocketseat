8cea4a4a121f3673870f8f313140cca40f5d10dd
Erick10Kito <erickcamposbem69@gmail.com>
2022-01-23 00:38:35 -0300

    Mudanças typescript firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,24 @@
+
+import firebase from 'firebase/compat/app';
+
+
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHsEJ-jq3cj2alCYD-8VTSN1WH9fGxmV8",
+  authDomain: "letmeask-4adc8.firebaseapp.com",
+  databaseURL: "https://letmeask-4adc8-default-rtdb.firebaseio.com",
+  projectId: "letmeask-4adc8",
+  storageBucket: "letmeask-4adc8.appspot.com",
+  messagingSenderId: "257037703765",
+  appId: "1:257037703765:web:af2f90d824c936cd2b92dc"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+export const auth = firebase.auth();
+export const database = firebase.database();
