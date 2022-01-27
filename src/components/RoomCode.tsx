72399396967d61d739d77ee2902b3cac3465ada0
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-01-27 18:00:19 -0300

    Termino Aula 3, perguntas sala

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,14 +1,23 @@
+import { type } from 'os'
 import copyImg from '../assets/images/copy.svg'
 import '../styles/room-code.scss'
 
 
-export function RoomCode() {
+type RoomCodeProps = {
+    code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(props.code)
+        //uso para copiar o código
+    }
     return (
-        <button className="room-code">
+        <button className="room-code" onClick={copyRoomCodeToClipboard}>
             <div>
                 <img src={copyImg} alt="copy room code" />
             </div>
-            <span>Sala #1221425685</span>
+            <span>Sala #{props.code}</span>
 
         </button>
     )
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,35 +1,144 @@
+
+import { type } from 'os';
+import { FormEvent, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 import '../styles/Room.scss';
 
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighLighted: boolean;
+}>
+
+
+type Question = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighLighted: boolean;
+}
+
+
+type RoomParams = {
+    id: string;
+}
+//Na linha do RoomCOde code ={params.id? params.id: ""} fiz isso pois o params vem com variavel indefinida , e como não é possivel declarar isso nesse type eu coloquei uma condição caso ela for indefinida nesse código
+
 export function Room() {
+    const { user } = useAuth();
+    const params = useParams<RoomParams>();
+    const [newQuestion, setNewQuestion] = useState('');
+    const [questions, setQuestions] = useState<Question[]>([]);
+    const [title, setTitle] = useState('')
+    const roomId = params.id;
+
+
+    useEffect(() => {
+        //o firebase retorna a pergunta como objeto ao inves de array e esse usoEffect faz com que ele retorne array
+        console.log(roomId)
+        const roomRef = database.ref(`rooms/${roomId}`);
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val();
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHighLighted: value.isHighLighted,
+                    isAnswered: value.isAnswered,
+                }
+            })
+
+            setTitle(databaseRoom.title);
+            setQuestions(parsedQuestions);
+
+
+        })
+    }, [roomId]);
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+        if (newQuestion.trim() === '') {
+            return;
+        }
+        if (!user) {
+            throw new Error('You must be logged in');
+        }
+
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user?.name,
+                avatar: user.avatar,
+            },
+            isHighLighted: false,
+            isAnswered: false
+        };
+
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+        setNewQuestion('');
+    }
+
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img className='logoImgElement' src={logoImg} alt="" />
-                    <RoomCode />
+                    <RoomCode code={params.id ? params.id : ""} />
+
                 </div>
+
             </header>
 
 
 
             <main className='content'>
                 <div className='room-title'>
-                    <h1>Sala React</h1>
-                    <span>4 perguntas</span>
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} Pergunta(s)</span>}
                 </div>
 
-                <form>
+                <form onSubmit={handleSendQuestion}>
                     <textarea
                         placeholder="O que você quer perguntar?"
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
                     />
                     <div className='form-footer'>
-                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-                        <Button type='submit'>Enviar Pergunta</Button>
+                        {user ? (
+                            <div className='user-info'>
+                                {questions.length !== 0 ? ('')
+                                    : (<><img src={user.avatar} alt={user.name} /><span className='name-auth'>{user.name}</span></>)
+                                }
+
+                            </div>
+
+                        ) : (<span>Para enviar uma pergunta, <button>faça seu login</button>.</span>)}
+
+                        <Button type='submit' disabled={!user} >Enviar Pergunta</Button>
                     </div>
                 </form>
+
+                {JSON.stringify(questions)}
             </main>
         </div>
     )
